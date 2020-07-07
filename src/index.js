51c221b56c26fe9aebbabffc6558fc0f96d46961
Eugene Kim <ekim1707@gmail.com>
2020-07-07 14:27:44 -0400

    adjusted favicon.png

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import 'semantic-ui-css/semantic.min.css';
 import './styles.scss';
 import Router from "./routes";
 
-//const App = () => <main className="mainContainer"><Router /></main>;
-//
-//ReactDOM.render(<App />, document.getElementById('root'));
 class App extends Component {
 	render() {
 		return (<main className="mainContainer"><Router /></main>);
